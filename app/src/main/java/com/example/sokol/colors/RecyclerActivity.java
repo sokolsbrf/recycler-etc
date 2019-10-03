9ab45c1746202049dad338f0630a73a20d2a5d98
dimasokol <me@dimasokol.ru>
2019-10-03 22:21:22 +0300

    Визуальное переключение между адаптерами

--- app/src/main/java/com/example/sokol/colors/RecyclerActivity.java
+++ app/src/main/java/com/example/sokol/colors/RecyclerActivity.java
@@ -1,9 +1,12 @@
 package com.example.sokol.colors;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class RecyclerActivity extends Activity {
 
@@ -18,5 +21,22 @@ public class RecyclerActivity extends Activity {
         recyclerView.setAdapter(new RecyclersColorsAdapter());
 
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_recycler, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_listview) {
+            Intent listActivity = new Intent(this, MainActivity.class);
+            startActivity(listActivity);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
